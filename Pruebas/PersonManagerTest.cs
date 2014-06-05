f4f8eee31657685f20284ef4eb22c1b83dced0df
IvanJMembrilla <ivanjmembrilla@hotmail.es>
2014-06-05 15:12:40 +0200

    prueba de conflictos

---
+++ Pruebas/PersonManagerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica03.Models;
+using Practica03;
+using System.Collections.Generic;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+         public void GetAllTest()
+        {
+            int elementos = PersonManager.GetAll().Count;
+
+            Person p = new Person();
+
+            p.EnrollmentDate = new DateTime();
+            p.HireDate = new DateTime();
+            p.FirstName = "Felipe";
+            p.LastName = "González";
+            PersonManager.Add(p);
+
+            int elementos2 = PersonManager.GetAll().Count;
+
+            Assert.AreEqual(elementos + 1, elementos2);
+
+            PersonManager.Remove(p.PersonID);
+        }
+    }
+}
